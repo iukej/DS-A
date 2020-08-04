70588cb713dd5b03c3aa0a16d95bd7884a5dfc2c
Ankur Kejriwal <ankurkejriwal@ankurs-mbp-3.lan>
2020-08-04 16:10:38 -0500

    2nd commit

---
+++ DS/LinkedList/LinkedList.cpp
@@ -0,0 +1,34 @@
+struct node{
+char data;
+node* next_node;
+}
+
+class linkedList{
+private:
+	node *tail, *head;
+public:
+	linkedList(){
+		tail = NULL;
+		head = NULL;
+	} 
+
+	int nextEntry(char input){
+		node* new_node;
+		new_node -> data = input;
+		new_node -> next_node = NULL;
+	
+		if(tail != NULL){
+			tail -> next_node = new_node;
+			tail = new_node;		
+		}
+		else{
+			tail = new_node;
+			head = new_node;	
+		}
+}
+
+
+int main(){
+	linkedList ll;
+	return 0;
+}
