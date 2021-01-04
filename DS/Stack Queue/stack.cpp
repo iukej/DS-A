b5d0ba2ab158c21b63c4104ec2c71237e5f4b03d
Ankur Kejriwal <ankurkejriwal@ankurs-mbp-3.lan>
2021-01-04 14:47:09 -0600

    stack written

--- DS/Stack Queue/stack.cpp
+++ DS/Stack Queue/stack.cpp
@@ -2,36 +2,59 @@
 
 struct node{
     node* prev_node;
-    char data[20];
+    //char data[20];
+    std::string data;
 };
 
 class Stack{
     private:
     public:
-        node* prior_node;
+        node* current_node;
         Stack(){
-            prior_node = NULL;
+            current_node = NULL;
         }
 
-        int insert_node(char input){
-            node* addition = new node();  //Of course I need to do this, If I don't have the new node part,
+        void push_node(std::string input){
+
+            node* prior_node = current_node;  //Store value so i can attach it to the new node
+            current_node = new node();
+            current_node->prev_node = prior_node;
+
+            //node* addition = new node();  //Of course I need to do this, If I don't have the new node part,
                                         //i've declared a pointer but never actually created the structure. obviously.
                                         //And that's why this works when there's no pointer
                                         //
                                         //Also, () after new initilizes with value in (). 0 if nothing.
 
-            std::cout << "node with following data inserted: " << input << std::endl;
-            return 1;
+            current_node->data = input;
+            std::cout << "node with following data inserted: " << current_node->data << std::endl;
+        };
+
+        void pop_node(){
+            std::cout << "node removed: " << current_node->data << std::endl;
+            current_node = current_node->prev_node;
+            std::cout << "new node top (prior node)" << current_node->data << std::endl;
+
+        }
+        
+        void print_current(){
+            std::cout << "current node value: " << current_node->data << std::endl;
         };
             
+
 };
 
 
 int main(){
     Stack dishes;
-    std::cout << "prior node value: " << dishes.prior_node << std::endl;
 
-    dishes.insert_node('h');
+    std::string first = "first node";
+    std::string second = "second node";
+    dishes.push_node(first);
+    dishes.push_node(second);
+    //dishes.print_current();
+    dishes.pop_node();
+
 
 
     return 1;
