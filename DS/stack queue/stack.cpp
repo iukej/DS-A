3f4ad9c3629b5cecd625feb3f97c955e6ab14d0c
Ankur Kejriwal <ankurkejriwal@ankurs-mbp-3.lan>
2021-01-04 15:35:50 -0600

    clean

--- DS/stack queue/stack.cpp
+++ DS/stack queue/stack.cpp
@@ -8,8 +8,8 @@ struct node{
 
 class Stack{
     private:
-    public:
         node* current_node;
+    public:
         Stack(){
             current_node = NULL;
         }
@@ -20,21 +20,16 @@ class Stack{
             current_node = new node();
             current_node->prev_node = prior_node;
 
-            //node* addition = new node();  //Of course I need to do this, If I don't have the new node part,
-                                        //i've declared a pointer but never actually created the structure. obviously.
-                                        //And that's why this works when there's no pointer
-                                        //
-                                        //Also, () after new initilizes with value in (). 0 if nothing.
-
             current_node->data = input;
             std::cout << "node with following data inserted: " << current_node->data << std::endl;
         };
 
         void pop_node(){
             std::cout << "node removed: " << current_node->data << std::endl;
-            current_node = current_node->prev_node;
+            node* prior_node_address = current_node->prev_node;
+            delete(current_node);
+            current_node = prior_node_address;
             std::cout << "new node top (prior node)" << current_node->data << std::endl;
-
         }
         
         void print_current(){
@@ -62,17 +57,3 @@ int main(){
 }
 
 
-
-//I think at compile time, you'd know globally instantiated variables, like a MAX_SIZE buffer way up top. But you wouldn't do this for, i.e., program input from a user using the program. You'd have to physically be running the program to get that input, so you can't decide how much space to allocate for it until run time. In that case, it totally makes sense to not allocate until you're physically executing the program.
-//
-//Also this allows you to do it within the heap as opposed to the stack, which gives you way more space to play with.
-
-//https://stackoverflow.com/questions/21350478/what-does-memory-allocated-at-compile-time-really-mean 
-//Has nice Explanations
-//
-
-
-
-//Questions:
-//Why do i have to allocate memory for the new node struct? Isn't everything in there known at compile time? A char array of 20 elements, and a pointer which would be 8 bytes? what more is there to know?
-//    Is the disctinction that the pointer is to an object of itself? I'm not sure what else it'd be.
