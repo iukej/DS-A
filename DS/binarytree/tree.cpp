a6ee0c02744380ddf3f549b4613ba7139e12fbb3
Ankur Kejriwal <ankurkejriwal@ankurs-mbp-3.lan>
2021-01-05 16:42:40 -0600

    tree with insert

--- DS/binarytree/tree.cpp
+++ DS/binarytree/tree.cpp
@@ -4,51 +4,81 @@ using namespace std;
 
 struct node{
     int key;
-    char value[20];
+    string value;
     node *parent;
     node *left_child;
     node *right_child;
 };
 
-struct childvalues{
-    int left_child_key;
-    int right_child_key;
-};
-
 class Tree{
     private:
-        node *root_node;
-        node *current_node;
     public:
-        Tree(){             //need constructor
+        node *root_node;
+        Tree(){                                                  //need constructor
             root_node = NULL;
-            current_node = root_node;
         };
 
-        childvalues ImmediateFamily(int currentKey, int searchKey){
-            childvalues children;
-            children.left_child_key = current_node->lef;
-            children.right_child_key = 2;
-            return children;
+        void insert_node(int key, string value){
+            if (not root_node){
+                root_node = new node;
+                root_node -> key = key;
+                root_node -> value = value;
+                return;
+            }
+            node* current_node = root_node;
+
+            bool placed = false;                        // W/o recursion for now
+            
+            while (not placed){
+                if ( key > current_node->key){                    //if keyval > existing node, check right subtree
+                    if (not current_node->right_child){           ////if empty, add it. if not traverse the new subtree
+                        current_node->right_child = new node;
+                        current_node->right_child->key = key;
+                        current_node->right_child->value = value; 
+                        placed = true;
+                    }else{
+                        current_node = current_node->right_child;
+                        continue;
+                    }
+                } else{
+                    if (not current_node->left_child){           //if keyval < exiting node, check left subtree
+                        current_node->left_child = new node;     // if empty, add it. if not traverse the new subtree
+                        current_node->left_child->key = key;
+                        current_node->left_child->value = value; 
+                        placed = true;
+                    }else{
+                        current_node = current_node->left_child;
+                        continue;
+                    }
+                }
+                
+
+            }
         };
 
-        //void insert_node(int key, char data){
-        //};
 };
 
 int main(){
     Tree spruce;
+    int root_key = 45;
+    string root_val = "first";
 
-    //childvalues testing;
+    spruce.insert_node(root_key, root_val);
+    cout << "root key: " << spruce.root_node -> key << endl;
+    cout << "root val: " << spruce.root_node -> value << endl;
+
+    int secondKey = 17;
+    string secondVal = "second";
+    spruce.insert_node(secondKey, secondVal);
 
 
-    int left_key, right_key;
-    childvalues children;
-    children =  spruce.ImmediateFamily(1,1);
-    left_key =  children.left_child_key;
-    right_key = children.right_child_key;
-    cout << "left key: " << left_key << endl;
-    cout << "right key: " << right_key << endl;
+    int thirdKey = 999;
+    string thirdval = "third";
+    spruce.insert_node(thirdKey, thirdval);
+
+    cout << "second: " << spruce.root_node->left_child->value << endl;
+    cout << "third: " << spruce.root_node->right_child->value << endl;
+
 
     return 1;
 };
